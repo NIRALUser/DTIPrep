97337f887d46642a6443e68886d2e3011dee40e4
Hans Johnson <hans-johnson@uiowa.edu>
2010-04-15 11:03:27 +0000

    COMP:  Initial conversion to CMake version 2.8 that will allow external packages to be included in build with dynamic cmake flags.

--- buildDTIPrep.sh
+++ buildDTIPrep.sh
@@ -2,9 +2,7 @@
 ##  This build tool was written by Hans J. Johnson hans-johnson@uiowa.edu
 
 PROJECTNAME=DTIPrep
-
 BUILDNAME=$(uname)_$(uname -m)-$(hostname -s)
-
 SOURCE_DIR=$(dirname $0)
 if [ "${SOURCE_DIR}" == "." ]; then
   SOURCE_DIR=$(pwd)
@@ -31,7 +29,6 @@ CC=gcc
 CXX=g++
 fi
 ## Removed -Wshadow from build to prevent qt warnings form hiding current errors
-FLAGS_FROM_QT_BUILD="-arch x86_64 -Xarch_x86_64 -mmacosx-version-min=10.5 -Wall -W "
 case ${ABI} in
   "PROFILE")
     CFLAGS="-Wall -Wstrict-prototypes -fprofile-arcs -ftest-coverage -pg  -UNDEBUG ${FLAGS_FROM_QT_BUILD}"
@@ -73,86 +70,8 @@ COMPILE_DIR=$(dirname ${SOURCE_DIR})/${PROJECTNAME}-COMPILE/
 ABI_DIR=${COMPILE_DIR}/$(uname)_$(uname -m)-${ABI}
 mkdir -p ${ABI_DIR}
 
-if [ 1 == 1 ];then  ## Temporary bypass of building ITK
-  LOCAL_PATH=$(dirname $0)
-  echo "${LOCAL_PATH}"
-  if [ "${LOCAL_PATH}" == "." ]; then
-    LOCAL_PATH=$(pwd)
-  fi
-  echo "${LOCAL_PATH}"
-  #################################################################################
-  #Get and build InsightToolkit-CVS
-  ITK_SOURCE=${COMPILE_DIR}/Insight
-  ITK_BUILD=${ABI_DIR}/InsightToolkit-CVS-build
-  if [ ! -f ${ITK_SOURCE}/CMakeLists.txt ] || [ ${LOCAL_PATH}/build${PROJECTNAME}.sh -nt ${ITK_SOURCE}/CMakeLists.txt ]; then
-    mkdir -p ${ITK_SOURCE}
-    pushd ${COMPILE_DIR}
-    cvs -d :pserver:anoncvs:@www.vtk.org:/cvsroot/Insight login
-    cvs -d :pserver:anoncvs@www.vtk.org:/cvsroot/Insight checkout -D 2009-10-15 Insight
-    popd
-  fi
-  mkdir -p ${ITK_BUILD}
-  pushd ${ITK_BUILD}
-  ##NOTE:  Using cmake and all comand line options.  Normally ccmake would be used.
-  CC=${CC} CXX=${CXX} CFLAGS=${CFLAGS} CXXFLAGS=${CXXFLAGS} cmake ${ITK_SOURCE} \
-    -DBUILD_EXAMPLES:BOOL=OFF \
-    -DBUILD_TESTING:BOOL=OFF \
-    -DBUILD_SHARED_LIBS:BOOL=OFF \
-    -DBUILDNAME:STRING=${ITK_BUILD_NAME} \
-    -DCOVERAGE_COMMAND:FILEPATH=/usr/bin/gcov-4.2 \
-    -DITK_USE_ORIENTED_IMAGE_DIRECTION:BOOL=ON \
-    -DITK_USE_REVIEW:BOOL=ON \
-    -DITK_USE_TRANSFORM_IO_FACTORIES:BOOL=ON \
-    -DITK_USE_OPTIMIZED_REGISTRATION_METHODS:BOOL=ON \
-    -DITK_USE_ORIENTED_IMAGE_DIRECTION:BOOL=ON \
-    -DITK_IMAGE_BEHAVES_AS_ORIENTED_IMAGE:BOOL=ON
-  if [ $? -ne 0 ]; then
-    echo "ERROR in configuring ITK"
-    exit -1
-  fi
-  make -j${maxproc}
-  popd
-fi  ## Temporary bypass of building ITK
-
-export QTDIR=/opt/qt-4.6-rc1
-export QTDIR=/opt/qt-4.5.2/
+export QTDIR=/opt/qt-4.6.2
 export PATH=${QTDIR}/bin:${PATH}
-  #################################################################################
-  #Get and build VTK-CVS
-  VTK_SOURCE=${COMPILE_DIR}/VTK
-  VTK_BUILD=${ABI_DIR}/VTK-CVS-build
-  if [ ! -f ${VTK_SOURCE}/CMakeLists.txt ] || [ ${LOCAL_PATH}/build${PROJECTNAME}.sh -nt ${VTK_SOURCE}/CMakeLists.txt ]; then
-    mkdir -p ${COMPILE_DIR}
-    pushd ${COMPILE_DIR}
-    cvs -d :pserver:anonymous:vtk@public.kitware.com:/cvsroot/VTK login
-    cvs -d :pserver:anonymous@public.kitware.com:/cvsroot/VTK checkout -D 2009-10-15 VTK
-    popd
-  fi
-  mkdir -p ${VTK_BUILD}
-  pushd ${VTK_BUILD}
-  ##NOTE:  Using cmake and all comand line options.  Normally ccmake would be used.
-  CC=${CC} CXX=${CXX} CFLAGS=${CFLAGS} CXXFLAGS=${CXXFLAGS} cmake ${VTK_SOURCE} \
-    -DBUILD_EXAMPLES:BOOL=OFF \
-    -DBUILD_TESTING:BOOL=OFF \
-    -DBUILD_SHARED_LIBS:BOOL=OFF \
-    -DBUILDNAME:STRING=${VTK_BUILD_NAME} \
-    -DVTK_USE_CARBON:BOOL=OFF \
-    -DVTK_USE_COCOA:BOOL=ON \
-    -DVTK_USE_QT:BOOL=ON \
-    -DVTK_USE_QVTK:BOOL=ON \
-    -DVTK_USE_GUISUPPORT:BOOL=ON \
-    -DQT_QMAKE_EXECUTABLE:FILEPATH=${QTDIR}/bin/qmake \
-    -DQT_SEARCH_PATH:FILEPATH=${QTDIR} \
-    -DDESIRED_QT_VERSION:STRING=4 \
-    -DCOVERAGE_COMMAND:FILEPATH=/usr/bin/gcov-4.2
-
-
-  if [ $? -ne 0 ]; then
-    echo "ERROR in configuring VTK"
-    exit -1
-  fi
-  make -j${maxproc}
-  popd
 
 #################################################################################
 #Build ${PROJECTNAME}
@@ -160,16 +79,13 @@ APP_DIR=${ABI_DIR}/${PROJECTNAME}
 mkdir -p ${APP_DIR}
 pushd ${APP_DIR}
 ##NOTE:  Using cmake and all comand line options.  Normally ccmake would be used.
-CMAKE_MODULE_PATH=${LOCAL_PATH}/CMake CC=${CC} CXX=${CXX} CFLAGS=${CFLAGS} CXXFLAGS=${CXXFLAGS} cmake ${SOURCE_DIR}/ -DBUILD_EXAMPLES:BOOL=ON -DBUILD_TESTING:BOOL=OFF -DBUILD_SHARED_LIBS:BOOL=OFF -DUSE_PRIVATE:BOOL=ON -DCOMPILE_ITKEMS:BOOL=ON -DVTK_DIR:PATH=${VTK_BUILD} -DITK_DIR:PATH=${ITK_BUILD} -DABI:STRING=${ABI} -DBUILDNAME:STRING=${ITK_BUILD_NAME} -DCOVERAGE_COMMAND:FILEPATH=/usr/bin/gcov-4.2 -DFLTK_DIR:PATH=${FLTK_BUILD} -DUSE_ITK_LIBRARY:BOOL=ON -DUSE_VTK_LIBRARY:BOOL=ON -DUSE_FLTK_LIBRARY:BOOL=ON \
+CMAKE_MODULE_PATH=${LOCAL_PATH}/CMake CC=${CC} CXX=${CXX} CFLAGS=${CFLAGS} CXXFLAGS=${CXXFLAGS} cmake ${SOURCE_DIR}/ -DBUILD_EXAMPLES:BOOL=ON -DBUILD_TESTING:BOOL=OFF -DBUILD_SHARED_LIBS:BOOL=OFF -DUSE_PRIVATE:BOOL=ON -DCOMPILE_ITKEMS:BOOL=ON -DVTK_DIR:PATH=${VTK_BUILD} -DITK_DIR:PATH=${ITK_BUILD} -DABI:STRING=${ABI} -DBUILDNAME:STRING=${ITK_BUILD_NAME} -DCOVERAGE_COMMAND:FILEPATH=/usr/bin/gcov-4.2 -DUSE_ITK_LIBRARY:BOOL=ON -DUSE_VTK_LIBRARY:BOOL=ON  \
                   -DCOMPILE_DISPLAY:BOOL=OFF \
                   -DBUILD_TESTING:BOOL=ON \
                   -DUSE_QT_LIBRARY:BOOL=ON \
                   -DQT_QMAKE_EXECUTABLE:FILEPATH=${QTDIR}/bin/qmake \
                   -DQT_SEARCH_PATH:FILEPATH=${QTDIR} \
                   -DDESIRED_QT_VERSION:STRING=4
-
-## NOTE: There is an interaction between COMPILE_DISPLAY and USE_QT_LIBRARY
-
 
 case "$(uname)" in
   "Linux")
@@ -184,5 +100,4 @@ case "$(uname)" in
 esac
 make
 popd
-
 
