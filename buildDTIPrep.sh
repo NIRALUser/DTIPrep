d01e51839b79c0d8a9eba6d848cc49b58b703ca7
Kent Williams <norman-k-williams@uiowa.edu>
2010-04-23 15:30:06 +0000

    cleaned up buildDTIPrep.sh -- but it is still a bit weird, and would only work on OS X

--- buildDTIPrep.sh
+++ buildDTIPrep.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 ##  This build tool was written by Hans J. Johnson hans-johnson@uiowa.edu
-
 PROJECTNAME=DTIPrep
 BUILDNAME=$(uname)_$(uname -m)-$(hostname -s)
 SOURCE_DIR=$(dirname $0)
@@ -13,21 +12,14 @@ if [ $# -lt 1 ]; then
 else
   ABI=$1
 fi
-ITK_BUILD_NAME=$(uname).$(uname -m).${ABI}
 
-## Valid types are Experimental, Continous, Nightly
-if [ $# -lt 2 ]; then
-  BUILDTYPE=Experimental
-else
-  BUILDTYPE=$2
-fi
-
-CC=/usr/bin/gcc-4.2
-CXX=/usr/bin/g++-4.2
+export CC=/usr/bin/gcc-4.2
+export CXX=/usr/bin/g++-4.2
 if [ ! -f ${CC} ] || [ ! -f ${CXX} ]; then
-CC=gcc
-CXX=g++
+export CC=`which gcc`
+export CXX=`which g++`
 fi
+
 ## Removed -Wshadow from build to prevent qt warnings form hiding current errors
 case ${ABI} in
   "PROFILE")
@@ -67,25 +59,30 @@ if [ "x${maxproc}" == "x" ] || [ ${maxproc} -lt 1 ]; then
 fi
 
 COMPILE_DIR=$(dirname ${SOURCE_DIR})/${PROJECTNAME}-COMPILE/
+echo COMPILE_DIR $COMPILE_DIR SOURCE_DIR $SOURCE_DIR
+
+FULL_SOURCE_PATH=$(readlink -f ${SOURCE_DIR})
+echo FULL_SOURCE_PATH $FULL_SOURCE_PATH
+
 ABI_DIR=${COMPILE_DIR}/$(uname)_$(uname -m)-${ABI}
 mkdir -p ${ABI_DIR}
 
 export QTDIR=/opt/qt-4.6.2
 export PATH=${QTDIR}/bin:${PATH}
-
+#export QTDIR=/opt/QtCarbon
+#export PATH=${QTDIR}/bin:${PATH}
 #################################################################################
 #Build ${PROJECTNAME}
-APP_DIR=${ABI_DIR}/${PROJECTNAME}
-mkdir -p ${APP_DIR}
-pushd ${APP_DIR}
+pushd ${ABI_DIR}
 ##NOTE:  Using cmake and all comand line options.  Normally ccmake would be used.
-CMAKE_MODULE_PATH=${LOCAL_PATH}/CMake CC=${CC} CXX=${CXX} CFLAGS=${CFLAGS} CXXFLAGS=${CXXFLAGS} cmake ${SOURCE_DIR}/ -DBUILD_EXAMPLES:BOOL=ON -DBUILD_TESTING:BOOL=OFF -DBUILD_SHARED_LIBS:BOOL=OFF -DUSE_PRIVATE:BOOL=ON -DCOMPILE_ITKEMS:BOOL=ON -DVTK_DIR:PATH=${VTK_BUILD} -DITK_DIR:PATH=${ITK_BUILD} -DABI:STRING=${ABI} -DBUILDNAME:STRING=${ITK_BUILD_NAME} -DCOVERAGE_COMMAND:FILEPATH=/usr/bin/gcov-4.2 -DUSE_ITK_LIBRARY:BOOL=ON -DUSE_VTK_LIBRARY:BOOL=ON  \
-                  -DCOMPILE_DISPLAY:BOOL=OFF \
-                  -DBUILD_TESTING:BOOL=ON \
-                  -DUSE_QT_LIBRARY:BOOL=ON \
-                  -DQT_QMAKE_EXECUTABLE:FILEPATH=${QTDIR}/bin/qmake \
-                  -DQT_SEARCH_PATH:FILEPATH=${QTDIR} \
-                  -DDESIRED_QT_VERSION:STRING=4
+CMAKE_MODULE_PATH=${LOCAL_PATH}/CMake CC=${CC} CXX=${CXX} CFLAGS=${CFLAGS} CXXFLAGS=${CXXFLAGS} \
+cmake \
+-DBUILD_SHARED_LIBS:BOOL=OFF \
+-DBUILD_TESTING:BOOL=OFF \
+-DQT_QMAKE_EXECUTABLE:FILEPATH=${QTDIR}/bin/qmake \
+-DQT_SEARCH_PATH:FILEPATH=${QTDIR} \
+-DDESIRED_QT_VERSION:STRING=4 \
+${FULL_SOURCE_PATH}
 
 case "$(uname)" in
   "Linux")
@@ -98,6 +95,6 @@ case "$(uname)" in
   echo "Platform not recognized"
   maxproc=1;
 esac
-make
-popd
+make -j ${maxproc}
+
 
