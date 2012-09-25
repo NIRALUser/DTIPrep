179768a4a0df2d9efdeae173fabf395c1546d192
Hans Johnson <hans-johnson@uiowa.edu>
2012-09-25 20:13:03 +0000

    ENH: Removed unnecessary macro.
    The MIN, MAX macros often conflict between code sources
    and should voided whenever possible.

--- src/DominantDirectional/Geometry_Common.h
+++ src/DominantDirectional/Geometry_Common.h
@@ -2,17 +2,12 @@
 #define GEOMETRY_COMMON_H
 
 /*
- *	By Yinpeng Li, mousquetaires@unc.edu
+ *  By Yinpeng Li, mousquetaires@unc.edu
  */
 
 namespace fiberodf{
 
 enum {X = 0, Y = 1, Z = 2} ;
-
-#define MIN(a, b) (((a) < (b))?(a) : (b))
-#define MAX(a, b) (((a) > (b))?(a) : (b))
-
 extern const double EPSILON ;
-
 }
 #endif
